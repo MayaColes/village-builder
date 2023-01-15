4fce00c9ac6abd995a3b0cb86ff8b02e46898a71
Maya Coles <mayacoles02@gmail.com>
2023-01-15 11:29:50 -0500

    Added storage service for managing localstorage

---
+++ src/services/storage.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  // Gets a value by name from local storage and initializes gameObject.
+  // Returns whether that value has been found
+  initObjFromLocalStorage(gameObject : any, findByValue : string){
+    let gameObjectInfo = localStorage.getItem(findByValue);
+
+    if(!gameObjectInfo){
+      return false;
+    }
+    else{
+      let gameObjectValues = JSON.parse(gameObjectInfo);
+
+      for(const key of gameObjectValues){
+        gameObject[key] = gameObjectValues[key]
+      }
+      return true;
+    }
+  }
+}
